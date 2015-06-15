2e68cd266568ec910fd9dfa4946d11b195f883cc
Winxuser <thewildturkey03@hotmail.com>
2015-06-15 22:48:17 +1000

    Update bluedroid build config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,9 +18,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy S III LTE"
-#define BLE_INCLUDED TRUE
-#define BTA_GATT_INCLUDED TRUE
-#define SMP_INCLUDED TRUE
+#define BTM_DEF_LOCAL_NAME "GT-I9305"
 
 #endif
