9c233f887a472a8dc630b0f7d21586b6f63ea17c
winxuser <jdownting31@gmail.com>
2016-01-02 21:55:59 +1000

    Fix Ril (Ported from T0lte)

--- i9305.mk
+++ i9305.mk
@@ -84,8 +84,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
-    ro.telephony.ril.config=exynos4RadioState
+    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bin/ks-wrapper.sh:/system/bin/ks
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
